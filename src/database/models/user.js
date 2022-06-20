c09d9b43553812f1fd7deb37e62229e1af2c9a11
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-06-20 18:25:50 -0300

    fix: Req2 model do user

---
+++ src/database/models/user.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true  },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  });
+  User.associate = (models) => {
+    User.hasMany(models.BlogPosts, {
+      foreignKey: 'userId',
+      as: 'BlogPosts',
+    });
+  }
+  return User;
+};
