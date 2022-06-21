90f70bff79ae703c24b0a3f51f04606a85d534c1
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-06-21 11:32:07 -0300

    feat:Req 2 model postCategory

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId:{ type: DataTypes.INTEGER, primaryKey: true },
+    categoryId:{ type: DataTypes.INTEGER, primaryKey: true },
+  });
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+      as: 'posts',
+    });
+    models.BlogPost.belongsToMany(models.Category, {
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+      as: 'categories',
+    });
+  }
+  return PostCategory;
+};
