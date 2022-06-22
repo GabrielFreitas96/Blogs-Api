ef7ea78eaa34a776b6d54b31efeb59d2d0f6c7ca
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-06-22 15:39:49 -0300

    feat: Req6 controller for get user by id

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -17,6 +17,15 @@ const addUser = async (req, res) => {
   res.status(201).json({ token: response });
 };
 
-const userController = { getAll, addUser };
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const response = await userService.getById(+id);
+  if (!response) {
+    return res.status(404).json({ message: 'User does not exist' });
+  }
+  res.status(200).json(response);
+};
+
+const userController = { getAll, addUser, getById };
 
 module.exports = userController;
