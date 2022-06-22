d70abcfd4aa87194e95265155f5f0b9cd0e6862e
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-06-22 16:18:27 -0300

    feat: Req9 service getall categories

--- src/services/categoriesService.js
+++ src/services/categoriesService.js
@@ -1,10 +1,15 @@
 const { Category } = require('../database/models');
 
+const getAll = async () => {
+  const categories = await Category.findAll();
+  return categories;
+};
+
 const addCategory = async (name) => {
   const category = await Category.create({ name });
   console.log('category no service', category);
   return category;
 };
 
-const categoriesService = { addCategory };
+const categoriesService = { addCategory, getAll };
 module.exports = categoriesService;
