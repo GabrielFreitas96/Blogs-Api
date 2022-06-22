88731d22da6dfc25d976de245d8fc023ff59c5a3
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-06-22 16:19:01 -0300

    feat: Req9 controller getall categories

--- src/controller/categoriesController.js
+++ src/controller/categoriesController.js
@@ -1,10 +1,15 @@
 const categoriesService = require('../services/categoriesService');
 
+const getAll = async (req, res) => {
+  const response = await categoriesService.getAll();
+  res.status(200).json(response);
+};
+
 const addCategory = async (req, res) => {
   const { name } = req.body;
   const response = await categoriesService.addCategory(name);
   res.status(201).json(response); 
 };
 
-const categoriesController = { addCategory };
+const categoriesController = { addCategory, getAll };
 module.exports = categoriesController;
