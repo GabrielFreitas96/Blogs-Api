738f4fadb9d91ae3d4c094f75975931684d048c2
GabrielFreitas96 <gabrielbfreitas96@gmail.com>
2022-06-23 11:11:25 -0300

    feat: Req15 put for post/:id

--- src/routers/routerPost.js
+++ src/routers/routerPost.js
@@ -3,10 +3,12 @@ const postController = require('../controller/postController');
 const verifyAuthentication = require('../middlewares/verifyAuthentication');
 const verifyEditPost = require('../middlewares/verifyEditPost');
 const verifyNewPost = require('../middlewares/verifyNewPost');
+const verifyUserEditPost = require('../middlewares/verifyUserEditPost');
 
 const routerPost = express.Router();
 routerPost.post('/', verifyAuthentication, verifyNewPost, postController.addPost);
 routerPost.get('/:id', verifyAuthentication, postController.getById);
 routerPost.get('/', verifyAuthentication, postController.getAll);
-routerPost.put('/:id', verifyAuthentication, verifyEditPost, postController.editPost);
+routerPost.put('/:id', verifyAuthentication, verifyEditPost,
+verifyUserEditPost, postController.editPost);
 module.exports = routerPost;
